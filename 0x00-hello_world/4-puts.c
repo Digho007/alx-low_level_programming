e6f03f397e2b01a075925691cc0dcaea1a1bcd61
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-13 14:16:30 +0000

    put program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main is a function that prints the string "Programming is like building a multilingual puzzle
+ * returns 0 to indicate successful
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
