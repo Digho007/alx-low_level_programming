8575e5557b458de7f0556bdc83c170aa5db82454
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-17 14:30:17 +0000

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a <= 99; a++)
+	for (a = '0'; a <= '99'; a++)
 	{
 		for (b = '0'; b <= '99'; b++)
 		{
@@ -20,7 +20,7 @@ int main(void)
 				putchar(' ');
 				putchar((b / 10) + '0');
 				putchar((b / 10) + '0');
-				if (a < 98 || b <= 99)
+				if (a < '98' || b <= '99')
 				{
 					putchar(',');
 					putchar(' ');
