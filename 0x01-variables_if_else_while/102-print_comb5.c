12915c2c055e30b68b9a1a03b8f0b0ba41702b71
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-17 11:11:11 +0000

    Updated readme

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two-digit numbers.
+ * Return: 0 means successful
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= 98; a++)
+	{
+		for (b = '1'; b <= '99'; b++)
+		{
+			if (a < b)
+			{
+
+			}
+		}
+	}
+}
