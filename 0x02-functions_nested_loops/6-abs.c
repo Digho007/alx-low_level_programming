cee18afef4a2eafc5c2510aa0e506d2e44bb7d3f
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-18 23:40:11 +0000

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,11 +7,10 @@
  * Return: 0 signifies success
  */
 
-int _abs(int)
+int _abs(int r)
 {
-	int r;
-
-	r = int * 1
-	printf("%d\n" r);
+	if (r < 0)
+		r *= -1;
+	printf("%d\n", r);
 	return (0);
 }
