0f7090d092318c14998bdc2b5351b52833ce101f
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-18 23:34:15 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @int: input
+ * Return: 0 signifies success
+ */
+
+int _abs(int)
+{
+	int r;
+
+	r = int * 1
+	printf("%d\n" r);
+	return (0);
+}
