3b24e129b70753c433cf50f4a6afe03173cd7554
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-19 01:32:14 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @a and @b are the integers
+ * Return: the resukt of the addition
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table stating with 0
+ */
+
+void times_table(void)
+{
+
+}
