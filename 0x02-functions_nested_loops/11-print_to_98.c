0849622e8f7c48e601622270152c974761fd8732
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-19 01:49:22 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: input
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+		_putchar(n);
+	_putchar(',');
+	_putchar(' ');
+	n++;
+	while (n > 98)
+		 _putchar(n);
+	_putchar(',');
+	_putchar(' ');
+	n--;
+}
