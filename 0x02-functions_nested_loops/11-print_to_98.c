7fe6f5b7e414b1e7e34c05368246fdfc98f5f1a3
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-19 01:56:25 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,17 @@
 void print_to_98(int n)
 {
 	while (n <= 98)
+	{
 		_putchar(n);
-	_putchar(',');
-	_putchar(' ');
+		_putchar(',');
+		_putchar(' ');
+	}
 	n++;
 	while (n > 98)
-		 _putchar(n);
-	_putchar(',');
-	_putchar(' ');
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
 	n--;
 }
