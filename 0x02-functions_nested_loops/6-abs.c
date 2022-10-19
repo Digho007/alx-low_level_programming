848b3835e9d9cf2cd850d7305ef0b7c804252c73
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-19 00:00:37 +0000

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * _abs - computes absolute value of an integer
  * @r: input
- * Return: absolute value of the input
+ * Return: 0 means successful
  */
 
 int _abs(int r)
 {
 	if (r < 0)
 		r *= -1;
-	printf("%d\n", r);
-	return (r);
+	_putchar(r);
+	return (0);
 }
