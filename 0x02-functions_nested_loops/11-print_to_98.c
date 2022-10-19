4a5a79462349dcd81278e6c207d26d0ef90f1f05
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-19 02:14:19 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,21 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (n = n; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
+		printf("98\n");
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		for (n = n; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
+		printf("98\n");
 	}
 
 }
