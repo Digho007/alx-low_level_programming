7b12a60e6f08b820bbb619dc36e85de4a1063caa
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-19 13:56:49 +0000

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,4 +5,20 @@
  * Return: 0 means successful
  */
 
-int main
+int main(void)
+{
+	int n, res;
+	int a = 1;
+	int b = 2;
+
+	printf("%d, ", a);
+	printf("%d, ", b);
+
+	for (n = 0; n <= 48; n++)
+	{
+		res = a + b;
+		printf("%d, ", res);
+	}
+	printf("\n");
+	return (0);
+}
