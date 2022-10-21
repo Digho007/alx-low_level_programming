7a890e915afb9a0a2607f68ffe7107cd563da7a9
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-21 02:48:11 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ void main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		printf("%d ", i);
-		if (i % 3 == 0)
-			printf("Fizz ");
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 	}
 }
