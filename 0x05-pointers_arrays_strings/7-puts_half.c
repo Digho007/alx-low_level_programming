4a4004b409ccaeb9b91d5af1e9f2c7337f174b45
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-24 22:31:14 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,20 +8,22 @@
 
 void puts_half(char *str)
 {
-	int i, j, k, len;
+	int i, j, len;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		len = i - 1;
-		if (len % 2 == 0)
-		{
-			for (j = (len / 2) + 1; j <= len; j++)
-				_putchar(str[j]);
-		}
-		if (len % 2 == 1)
-		{
-			for (k = ((len - 1) / 2) + 1; k <= len; k++)
-				_putchar(str[k]);
-		}
+	if (len % 2 == 0)
+	{
+		j = len / 2;
 	}
+	else
+	{
+		j = (len - 1) / 2;
+	}
+	while (j < len)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
 }
