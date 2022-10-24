cdbb80ff33da69779512c0000bc9d30a3d9dc840
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-24 19:39:07 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input; *s: pointer to a string
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = o; s[i] != '\0'; i++)
+		i += i;
+	return (i);
+}
