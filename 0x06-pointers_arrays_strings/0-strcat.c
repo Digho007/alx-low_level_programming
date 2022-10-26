9398c5d6b6502beef6a1bacb8a6a734cd086fc9c
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-26 14:44:22 +0000

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,6 +4,7 @@
  * *_strcat - concatenates two strings
  * @dest: destination
  * @src: source
+ * Return: destination
  */
 
 char *_strcat(char *dest, char *src);
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strncat - function concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+}
