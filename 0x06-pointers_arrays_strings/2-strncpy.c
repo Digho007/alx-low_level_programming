c0cdafc992b30cf75178d5b9ed831098da111de4
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-27 08:49:41 +0000

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,4 +8,14 @@
  * Return: destination string
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+		dest[j] = src[j];
+	while (j < n)
+		dest[i++] = '\0';
+	return (dest);
+}
 
