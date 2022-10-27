b6334ca655c30ae759dd380c5e098b6dbb0f35b4
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-27 08:28:34 +0000

    task 0

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function copies strings
+ * @dest: destination string
+ * @src: source string
+ * @n: numbers to be copied
+ * Return: destination string
+ */
+
+
