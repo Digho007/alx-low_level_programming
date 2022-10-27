b5996587ef15140cc3250daa1f44411933a90503
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-27 11:21:31 +0000

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - compares two strings
  * @s1: First string
  * @s2: Second string
- * Return: 0 always successful
+ * Return: 0 is s1 == s2; less than 0 if s1 < s2 and greater than 0 if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
@@ -12,6 +12,6 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
-		return (0);
+		;
 	return (s1[i] - s2[i]);
 }
