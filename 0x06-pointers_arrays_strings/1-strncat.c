0cf42a5ae4efd7dee94440e167bbe6aff2620f1d
Digho007 <dighomanorjeremiah@gmail.com>
2022-10-27 08:19:34 +0000

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,10 +4,19 @@
  * *_strncat - function concatenates two strings
  * @dest: destination
  * @src: source
+ * @n: number of bytes
  * Return: destination
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0' && j < n; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+
+	return (dest);
 }
