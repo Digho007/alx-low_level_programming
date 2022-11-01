21161ca2ebc7953f1fe7adfad6a39af1b8a2210c
Digho007 <dighomanorjeremiah@gmail.com>
2022-11-01 11:35:21 +0000

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies bytes from source to destination
+ * @n: number of bytes to be copied
+ * @src: source
+ * @dest: destination
+ * Return: pointer to destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n - 1, i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
