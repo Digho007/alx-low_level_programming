c7e14ce15b0bcce232ca97e4014be72808b11a8e
Digho007 <dighomanorjeremiah@gmail.com>
2022-11-15 22:30:31 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * string_nconcat - concatenate 2 strings, only n bytes of s2
@@ -12,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	int strlen1, i, c;
+	int strlen1, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -23,12 +24,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	for (i = 0; i < (strlen1 + n); i++)
 	{
 		if (i < strlen1)
 			p[i] = s1[i];
 		else
-			p[i] = s2[c++];
+			p[i] = s2[(strlen1 + n) - i];
 	}
 	p[i] = '\0';
 
