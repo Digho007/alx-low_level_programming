89a5ce2400828f3a236f16a9b3cae8f69801f149
Digho007 <dighomanorjeremiah@gmail.com>
2022-11-25 07:48:06 +0000

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; h != NULL; i++)
+	for (i; h != NULL; i++)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
