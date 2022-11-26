28dfae8ab0036610c1aa87dbdb1e626ee7dcc915
Digho007 <dighomanorjeremiah@gmail.com>
2022-11-26 03:22:25 +0000

    task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section	.text
-   global _start     ;must be declared for linker (ld)
+   global _start
 	
-_start:	            ;tells linker entry point
-   mov	edx,len     ;message length
-   mov	ecx,msg     ;message to write
-   mov	ebx,1       ;file descriptor (stdout)
-   mov	eax,4       ;system call number (sys_write)
-   int	0x80        ;call kernel
+_start:    
+   mov	edx,len
+   mov	ecx,msg
+   mov	ebx,1
+   mov	eax,4
+   int	0x80
 	
-   mov	eax,1       ;system call number (sys_exit)
-   int	0x80        ;call kernel
+   mov	eax,1
+   int	0x80
 
 section	.data
-msg db 'Hello, Holberton', 0xa  ;string to be printed
-len equ $ - msg     ;length of the string
+msg db 'Hello, Holberton', 0xa
+len equ $ - msg
